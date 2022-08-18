5413537f004eedbe3a50fccc8043fe28675bbfd8
TurksoyAz <supercoder@AirvonSperCoder.fritz.box>
2022-08-18 23:17:17 +0200

    HG

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+
+function tikla1() {
+  ha1.style.color = "rgb(103,12,83)";
+  bodo.style.backgroundColor = "rgb(0,132,78)";
+}
+
+function tikla2() {
+  ha1.style.color = "rgb(216,161,69)";
+  bodo.style.backgroundColor = "rgb(60,57,90)";
+}
+
+function tikla3() {
+  ha1.style.color = "rgb(198,82,28)";
+  bodo.style.backgroundColor = "rgb(1,0,27)";
+}
+
+function tikla4() {
+  ha1.style.color = "rgb(11,5,70)";
+  bodo.style.backgroundColor = "rgb(0,198,250)";
+}
+
